0a01ecf70863a50add03372585d6ef9ed3bb7d90
Victor Ross <vicjun22@gmail.com>
2023-04-23 23:42:15 -0300

    upgrade rules

--- web/src/ui/screens/rules/rules.screen.tsx
+++ web/src/ui/screens/rules/rules.screen.tsx
@@ -1,10 +1,18 @@
-import { rules } from '../../../utils/rules';
 import './rules.style.scss';
 
+import { useNavigate } from 'react-router-dom';
+
+import { rules } from '../../../utils/rules';
+import { Button } from '../../components';
+import { ROUTES } from '../../../constants';
+
 export function Rules() {
-    const titulos = Object.values(rules).map((regras: any) => {
+    const navigate = useNavigate();
+    const titulos = Object.values(rules).map((regras: any, index: number) => {
         return (
-            <li>{regras.title}</li>
+            <li className={'regras_titulos'} key={index}>
+                {regras.title}
+            </li>
         )
     })
 
@@ -12,7 +20,10 @@ export function Rules() {
     return (
         <section className={'section_rules'}>
             <div className={'section_rules_container'}>
-                {titulos}
+                <ul className={'regras_titulos_container'}>
+                    {titulos}
+                </ul>
+                <Button onClick={() => navigate(ROUTES.HOME_PAGE.path)}>Voltar</Button>
             </div>
 
             <div className={'section_rules_content'}>
