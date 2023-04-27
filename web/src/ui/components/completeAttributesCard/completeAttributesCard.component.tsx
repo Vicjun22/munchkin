5c6d2fb90683875bc7da88bdbf9e92c6673f6729
Victor Ross <vicjun22@gmail.com>
2023-04-27 00:38:45 -0300

    Update Game

--- web/src/ui/components/completeAttributesCard/completeAttributesCard.component.tsx
+++ web/src/ui/components/completeAttributesCard/completeAttributesCard.component.tsx
@@ -1,15 +1,32 @@
 import './completeAttributesCard.style.scss';
 
-import { Card } from '../card/card.component';
+import HumanF from '../../../assets/images/munchkin_human-female.svg'
+import HumanM from '../../../assets/images/munchkin_human-male.svg'
 
 export function CompleteAttributesCard({player, setOrdemAtual}: any) {
+    const imagem: any = {
+        'Humano-feminino': HumanF,
+        'Humano-masculino': HumanM
+  }
 
     return (
-        <div className={'current-player_card'}>
-            <div className={'current-player_card_container'}>
-                <Card personagem={player} qtdPlayers={'um-jogador'} />
-                
-                <div className={'current-player_card_attributes'}>
+        <div className={'cac_current-player'}>
+            <div className={'cac_current-player_container'}>
+                <div className={'cac_current-player-info'}>
+                    <p className={'cac_nivel'}>{player.nivel}</p>
+                    <p className={'cac_nome'}>{player.nome}</p>
+                    <img
+                        src={imagem[player.racaGenero]}
+                        alt={'Imagem do jogador'}
+                        className={'cac_img'}
+                    />
+                    <div className={'cac_raca-classe'}>
+                        <p>Raça: {player.duasRacas ? player.raca1 + '/' + player.raca2 : player.raca1}</p>
+                        <p>Classe: {player.classe || 'Nenhum'}</p>
+                    </div>
+                </div>
+
+                <div className={'cac_current-player-custom'}>
 
                 </div>
             </div>
