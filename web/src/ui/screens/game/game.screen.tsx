7f5f0f23d3d779f29066d12212a93205766c5a47
Victor Ross <vicjun22@gmail.com>
2023-05-15 16:11:20 -0300

    Update Game

--- web/src/ui/screens/game/game.screen.tsx
+++ web/src/ui/screens/game/game.screen.tsx
@@ -4,14 +4,22 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { ROUTES } from '../../../constants';
-import { CompleteAttributesCard } from '../../components';
+import { Title } from '../../components';
 import { PlayerType } from '../../../types';
+
+import HumanF from '../../../assets/images/munchkin_human-female.svg'
+import HumanM from '../../../assets/images/munchkin_human-male.svg'
 
 export function GameScreen({ qtdPlayers, players, setPlayers, gameStorage }: any) {
     const [ordemAtual, setOrdemAtual] = useState<number>(0);
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    
+    const playerAtual: any = players.find((player: PlayerType) => player.ordem === ordemAtual);
 
-    const playerAtual: PlayerType = players.find((player: PlayerType) => player.ordem === ordemAtual);
+    const imagem: any = {
+        'Humano-feminino': HumanF,
+        'Humano-masculino': HumanM
+    }
 
     useEffect(() => {
         if (!gameStorage) {
@@ -19,14 +27,26 @@ export function GameScreen({ qtdPlayers, players, setPlayers, gameStorage }: any
         }
     }, [])
 
+    // ver completeAttributesCard.component e depois apagar...
+
     return (
         <section className={'section_game'}>
-            <CompleteAttributesCard
-                player={playerAtual}
-                players={players}
-                setPlayers={setPlayers}
-                setOrdemAtual={setOrdemAtual}
-            />
+            <Title />
+
+            <div className={'section_game_container'}>
+                <div className={'section_game_img-div'}>
+                    
+                    <img
+                        src={imagem[playerAtual.racaGenero]}
+                        alt={'Imagem do jogador'}
+                        className={'section_game_img'}
+                    />
+                </div>
+
+                <div className={'section_game_data-div'}>
+
+                </div>
+            </div>
         </section>
     )
 }
